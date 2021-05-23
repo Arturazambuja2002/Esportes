8452bd1b311c7ec227884bb836b80a3941e5e3f0
Arturazambuja2002 <arturazambuja2002@gmail.com>
2021-05-23 11:31:05 -0300

    Commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect:"postgres",
+    host:"localhost",
+    port:"5000",
+    username:"postgres",
+    password:"postgres",
+    database:"esportes",
+    define:{
+        timestemps:true
+    }
+}
